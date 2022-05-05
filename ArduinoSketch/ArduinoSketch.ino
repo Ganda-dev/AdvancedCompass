6888a79432d27b47c29e132ea9073a8d77a21e3e
Ganda <gioganda@hotmail.com>
2022-05-05 17:45:12 +0200

    gpsSetup

--- ArduinoSketch/ArduinoSketch.ino
+++ ArduinoSketch/ArduinoSketch.ino
@@ -1,9 +1,16 @@
+#include "gps.h" //gps
+//#include "lcd.h" //lcd(opzionale)
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  GPSSetup();
+  //LCDSetup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  getInfo();
+  
+  
+  
+  printGPS(); //per stampare i valori nel monitor seriale(115200)
 }
--- ArduinoSketch/gps.h
+++ ArduinoSketch/gps.h
@@ -1,1 +1,31 @@
+#include <Arduino.h>
+#include <TinyGPS.h>
+#include <SoftwareSerial.h>
 
+float lat = 0.0000;
+float lon = 0.0000;
+SoftwareSerial gpsSerial(2,3); //RX, TX
+TinyGPS gps; //Creo l'oggetto GPS
+
+void GPSSetup(){
+  gpsSerial.begin(9600);
+}
+
+void getInfo(){
+    while(gpsSerial.available()){
+      if(gps.encode(gpsSerial.read())){
+        gps.f_get_position(&lat,&lon);  
+      }
+    }
+}
+
+void printGPS(){
+  while(gpsSerial.available()){
+    if(gps.encode(gpsSerial.read())){
+          Serial.println("lat:"+String(lat,5)+";");
+          Serial.println("lon:"+String(lon,5)+";");
+          Serial.println("----");
+          delay(50); 
+      }
+  }
+}
